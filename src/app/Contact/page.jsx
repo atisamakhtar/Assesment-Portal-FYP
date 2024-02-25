23153143efd2239fe200ead2b725092f9c6009a5
atisamakhtar <atisam.akhtar@gmail.com>
2024-02-26 00:43:33 +0500

    contact page somehow added

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import Navbar from "../Navbar/navbar";
-import Footer from "../Footer/footer";
 import CSection from "./CSection";
 
 function Contact() {
   return (
     <>
-    <Navbar/>
-    <CSection/>
-    <Footer/>
-
-</>
+      <CSection />
+    </>
   );
 }
 
