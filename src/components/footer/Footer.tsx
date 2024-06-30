e5fbb0fe6a9bdb0f68db46db0c479f0ff7335143
atisamakhtar <atisam.akhtar@gmail.com>
2024-06-30 14:28:06 +0500

    footer updated

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({ }: Props) {
 
   return (
     <>
-      <footer className={`static bottom-0 left-0 w-full text-center py-3 text-white bg-slate-900`} >&copy; 2024, All rights reserved</footer>
+      <footer className={`static bottom-0 left-0 w-full text-center py-3 text-white bg-slate-900`} >&copy; 2024, All rights reserved | Self Assessment Portal</footer>
     </>
   )
 }
